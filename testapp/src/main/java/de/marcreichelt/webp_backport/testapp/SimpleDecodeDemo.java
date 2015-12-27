67e92cd6aa88044787e1b35140c68d59909d7754
Marc Reichelt <mcreichelt@gmail.com>
2015-12-27 18:31:41 +0100

    removed unused imports

--- testapp/src/main/java/de/marcreichelt/webp_backport/testapp/SimpleDecodeDemo.java
+++ testapp/src/main/java/de/marcreichelt/webp_backport/testapp/SimpleDecodeDemo.java
@@ -5,8 +5,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
